0bc67b32db9beb3af671f0640ab170d2cf6bc5f7
Yisus <jesusgameroom@gmail.com>
2023-08-15 17:44:46 -0400

    using configs in config file

--- src/commands.ts
+++ src/commands.ts
@@ -1,6 +1,8 @@
 import { program } from 'commander';
+import fs from 'fs-extra';
+import path from 'path';
 import server from './server.js';
-import { initialize } from './templates/templates.js'; 
+import { initialize, ExecPHPConfigJson } from './templates/templates.js'; 
 
 program
     .version('1.0.0')
@@ -8,14 +10,23 @@ program
 
 program.command('init <path>')
     .description('Creates configuration files and folders')
-    .action((path: string) => {
-        initialize(path);
+    .action((dirPath: string) => {
+        initialize(dirPath);
     });
 
 program.command('up')
     .description("Start the HTTP server and serve your app")
     .action(() => {
-        server();
+        console.log("Looking for config file...");
+        const configFile = path.join(process.cwd(), 'execphp.config.json');
+        if (!fs.existsSync(configFile)) {
+            program.error("ERR! Couldn't find a config file.");
+        }
+
+        console.log("Found.");
+        const configs = JSON.parse(fs.readFileSync(configFile, { encoding: 'utf-8' })) as ExecPHPConfigJson;
+        
+        server(configs.port, path.join(process.cwd(), configs.outDir));
     });
 
 program.parse();
